a933c002c48f50f5416881d4889df4c4723cd7d4
Daniel Malone <danieljmalone@gmail.com>
2017-05-13 19:38:02 -0500

    add email, address

--- app/src/main/java/com/finepointmobile/okhttptest2/MainAdapter.java
+++ app/src/main/java/com/finepointmobile/okhttptest2/MainAdapter.java
@@ -33,7 +33,10 @@ class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
     public void onBindViewHolder(MainAdapter.ViewHolder holder, int position) {
         try {
             JSONObject jsonArray = new JSONObject(mDataset.get(position));
+            JSONObject address = jsonArray.getJSONObject("address");
             holder.mTitle.setText(jsonArray.getString("name"));
+            holder.mEmail.setText(jsonArray.getString("email"));
+            holder.mAddress.setText(address.getString("street"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -47,10 +50,14 @@ class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTitle;
+        public TextView mEmail;
+        public TextView mAddress;
 
         public ViewHolder(View itemView) {
             super(itemView);
             mTitle = (TextView) itemView.findViewById(R.id.title);
+            mEmail = (TextView) itemView.findViewById(R.id.email);
+            mAddress = (TextView) itemView.findViewById(R.id.address);
         }
     }
 }
